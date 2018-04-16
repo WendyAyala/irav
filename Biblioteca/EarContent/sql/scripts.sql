88f6bd65719f6a57723bb7c133c21dea62d70a8f
wendy.ayala <wendy.ayala@estudiante.uam.es>
2018-04-16 23:42:06 +0200

    scripts

---
+++ Biblioteca/EarContent/sql/scripts.sql
@@ -0,0 +1,9 @@
+#aumenta una columa en la tabla libro tipo character varaying 50 llamado lib_imagen
+#Ingresa un autor y editorial cualquiera con id 1
+
+insert into public.libro values ('2',10,'Carrie','1','1','Terror','carrie.jpg')
+insert into public.libro values ('3',5,'Apocalipsis','1','1','Terror','apocalipsis.jpg')
+
+insert into public.libro values ('4',7,'El Ciclo del Hombre Lobo','1','1','Terror','lobo.jpg');
+insert into public.libro values ('5',8,'IT','1','1','Terror','it.jpg');
+insert into public.libro values ('6',4,'Cupula','1','1','Terror','under.jpg');
